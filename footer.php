8d2bb2f9730d260ab0419f03291a7019e8e557ce
jmrpadrino <jmrpadrino@gmail.com>
2017-11-28 21:51:14 -0400

    Rehaciendo el footer

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
 <footer>
     <div class="container-fluid">
-        <div class="row">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/footer.jpg" title="Disfruta Orangine!" alt="Orangine Footer" class="img-responsive">
+        <div class="row" style="background: #E55100; color: white;">
+            <div class="col-xs-3"><h3>Columna 1</h3></div>
+            <div class="col-xs-3"><h3>Columna 2</h3></div>
+            <div class="col-xs-6"><h3>Columna 3</h3></div>
+        </div>
+        <div class="row" style="background: blue; color: white;">
+            <div class="col-xs-3"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil laudantium saepe placeat dolores aspernatur, delectus, distinctio, temporibus quibusdam, iusto molestiae est error voluptas! Beatae omnis aliquid, temporibus laboriosam modi explicabo!</p></div>
+            <div class="col-xs-3"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque deleniti doloribus sed eaque odio accusamus doloremque alias incidunt nulla fugit architecto corporis nemo unde ipsam et placeat, ducimus, laboriosam facere.</p></div>
+            <div class="col-xs-6"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos enim ex quae totam molestiae, similique maxime eius laboriosam explicabo beatae mollitia sequi libero, ab saepe, incidunt nulla iste voluptatum molestias.</p></div>
         </div>
     </div>
 </footer>
